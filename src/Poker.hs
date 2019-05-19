d41892d49a0f573b52ccd24b98e9535c872158e6
Jun Nakajima <ikuta1919@gmail.com>
2019-05-19 18:17:05 +0900

    [mod] とりあえずグリーンにした

--- src/Poker.hs
+++ src/Poker.hs
@@ -13,7 +13,7 @@ instance Ord Rank where
 instance Ord Card where
   compare = compare `on` rank
 
-data Hand =  HighCard | Flush | Straight | Pair | StraightFlush
+data Hand =  HighCard | Flush | Straight | Pair | StraightFlush | ThreeCard
   deriving (Show, Ord, Eq)
 
 newtype TwoCardHand = TwoCardHand { fromTwoCardHand :: Hand } deriving (Eq)
@@ -30,22 +30,45 @@ instance Ord TwoCardHand where
 hand :: Cards -> Hand
 hand (Cards first second)
   | isStraight && isFlush = StraightFlush
-  | isPair = Pair
+  | isPair [first, second] = Pair
   | isStraight = Straight
   | isFlush = Flush
   | otherwise = HighCard
   where
-    isPair = first `hasSameRank` second
     isFlush = first `hasSameSuit` second
     isStraight = rank first `isConsecutiveRank` rank second
-  
+
+threeHand :: ThreeCards -> Hand 
+threeHand (ThreeCards first second third) 
+  | isThreeCard = ThreeCard
+  | isStraight = Straight
+  | isFlush = Flush
+  | isPair [first,second,third] = Pair
+  | otherwise = HighCard
+  where 
+    isThreeCard = all (hasSameRank first) [second,third]
+    isFlush = all (hasSameSuit first) [second,third]
+    isStraight = isConsecutiveThreeRank (rank first) (rank second) (rank third)
+
+isPair :: [Card] -> Bool
+isPair cards = or [ x `hasSameRank` y | x <- cards, y <- cards, x /= y]
+
 isConsecutiveRank :: Rank -> Rank -> Bool
 isConsecutiveRank King Ace = True
 isConsecutiveRank Ace King = True
 isConsecutiveRank first second = abs (fromEnum first - fromEnum second) == 1
 
+isConsecutiveThreeRank :: Rank -> Rank -> Rank -> Bool 
+isConsecutiveThreeRank first second third = 
+  let sorted = map sort $ order first second third
+  in or $ map (hoge (\l r -> r - l == 1)) sorted  
+
 data Cards = Cards Card Card
   deriving (Show, Eq)
+
+data ThreeCards = ThreeCards Card Card Card
+  deriving (Show, Eq)
+
 
 instance Ord Cards where
   compare left right = case (compare `on` TwoCardHand . hand) left right of
@@ -60,3 +83,24 @@ compareCardsWithRank = compare `on` sortCards
       [ace@(Card _ Ace), two@(Card _ Two)] -> [two, ace]
       other -> other
       where descSortedCards = reverse . sort $ [left, right]
+
+orderCandidates :: Rank -> [Int]
+orderCandidates Ace = [1, 14]
+orderCandidates x = [fromEnum x + 1]
+
+isStraight :: [Int] -> Bool
+isStraight ranks = last sorted - head sorted == 2
+  where 
+    sorted = sort ranks
+
+order f s t = do
+  f' <- orderCandidates f
+  s' <- orderCandidates s 
+  t' <- orderCandidates t
+  return [f', s', t']
+
+hoge :: (a -> a -> Bool) -> [a] -> Bool
+hoge f [left, right] = f left right
+hoge f (left:right:rest)
+  | f left right = hoge f (right:rest)
+  | otherwise = False
--- test/PokerSpec.hs
+++ test/PokerSpec.hs
@@ -14,47 +14,71 @@ spec = do
       , c_aD, c_2D, c_3D, c_4D, c_5D, c_6D, c_7D, c_8D, c_9D, c_10D, c_jD, c_qD, c_kD]
       = [Card suit rank | suit <- [Spade, Heart, Club, Diamond], rank <- [Ace .. King]]
 
-  describe "ツーカードのペアの役" $ do
-    it "7♠と8♠ はストレートフラッシュである" $ hand (Cards c_7S c_8S) `shouldBe` StraightFlush
-    it "A♠とA♥ はペアである" $ hand (Cards c_aS c_aH) `shouldBe` Pair
-    it "K♠とA♥ はストレートである" $ hand (Cards c_kS c_aH) `shouldBe` Straight
-    it "7♦と8♠ はストレートである" $ hand (Cards c_7D c_8S) `shouldBe` Straight
-    it "A♠と7♠ はフラッシュである" $ hand (Cards c_aS c_7S) `shouldBe` Flush
-    it "7♦とA♥ はハイカードである" $ hand (Cards c_7D c_aH) `shouldBe` HighCard
+  describe "ツーカード" $ do 
+    describe "役の判定" $ do
+      it "7♠と8♠ はストレートフラッシュである" $ hand (Cards c_7S c_8S) `shouldBe` StraightFlush
+      it "A♠とA♥ はペアである" $ hand (Cards c_aS c_aH) `shouldBe` Pair
+      it "K♠とA♥ はストレートである" $ hand (Cards c_kS c_aH) `shouldBe` Straight
+      it "7♦と8♠ はストレートである" $ hand (Cards c_7D c_8S) `shouldBe` Straight
+      it "A♠と7♠ はフラッシュである" $ hand (Cards c_aS c_7S) `shouldBe` Flush
+      it "7♦とA♥ はハイカードである" $ hand (Cards c_7D c_aH) `shouldBe` HighCard
 
-  describe "手札の強さを比較する" $ do 
-    describe "役が違う場合は、役の強さで強弱が決まる" $ do
-      describe "ストレートフラッシュ > ペア" $ do
-        it "A♠/K♠ > A♦/A♥" $ Cards c_aS c_kS > Cards c_aD c_aH `shouldBe` True
-      describe "ペア > ストレート" $ do
-        it "2♠/2♥ > A♦/K♣" $ Cards c_2S c_2H > Cards c_aD c_kC `shouldBe` True
-      describe "ストレート > フラッシュ" $ do
-        it "A♠/2♥ > A♦/3♦" $ Cards c_aS c_2H > Cards c_aD c_3D `shouldBe` True
-      describe "フラッシュ > ハイカード" $ do
-        it "2♠/4♠ > A♦/Q♣" $ Cards c_2S c_4S > Cards c_aD c_qC `shouldBe` True
+    describe "手札の強さを比較する" $ do 
+      describe "役が違う場合は、役の強さで強弱が決まる" $ do
+        describe "ストレートフラッシュ > ペア" $ do
+          it "A♠/K♠ > A♦/A♥" $ Cards c_aS c_kS > Cards c_aD c_aH `shouldBe` True
+        describe "ペア > ストレート" $ do
+          it "2♠/2♥ > A♦/K♣" $ Cards c_2S c_2H > Cards c_aD c_kC `shouldBe` True
+        describe "ストレート > フラッシュ" $ do
+          it "A♠/2♥ > A♦/3♦" $ Cards c_aS c_2H > Cards c_aD c_3D `shouldBe` True
+        describe "フラッシュ > ハイカード" $ do
+          it "2♠/4♠ > A♦/Q♣" $ Cards c_2S c_4S > Cards c_aD c_qC `shouldBe` True
 
-    describe "役が同じ場合は、ランクの強弱で決まる" $ do
-      describe "ストレートフラッシュ同士の比較" $ do
-        it "A♠/K♠ > K♥/Q♥" $ Cards c_aS c_kS > Cards c_kH c_qH `shouldBe` True
-        it "2♠/3♠ > A♥/2♥" $ Cards c_2S c_3S > Cards c_aH c_2H `shouldBe` True
-        it "A♠/K♠ と A♥/K♥ は引き分け" $ Cards c_aC c_kC `compare` Cards c_aH c_kH `shouldBe` EQ
+      describe "役が同じ場合は、ランクの強弱で決まる" $ do
+        describe "ストレートフラッシュ同士の比較" $ do
+          it "A♠/K♠ > K♥/Q♥" $ Cards c_aS c_kS > Cards c_kH c_qH `shouldBe` True
+          it "2♠/3♠ > A♥/2♥" $ Cards c_2S c_3S > Cards c_aH c_2H `shouldBe` True
+          it "A♠/K♠ と A♥/K♥ は引き分け" $ Cards c_aC c_kC `compare` Cards c_aH c_kH `shouldBe` EQ
 
-      describe "ペア同士の比較" $ do
-        it "2♥/2♠ < 3♥/3♠" $ Cards c_2H c_2S > Cards c_3H c_3S `shouldBe` False
-        it "A♥/A♠ > 2♥/2♠" $ Cards c_aH c_aS > Cards c_2H c_2S `shouldBe` True
-        it "3♥/3♠ と 3♦/3♣ は引き分け" $ Cards c_3H c_3S `compare` Cards c_3D c_3C `shouldBe` EQ
+        describe "ペア同士の比較" $ do
+          it "2♥/2♠ < 3♥/3♠" $ Cards c_2H c_2S > Cards c_3H c_3S `shouldBe` False
+          it "A♥/A♠ > 2♥/2♠" $ Cards c_aH c_aS > Cards c_2H c_2S `shouldBe` True
+          it "3♥/3♠ と 3♦/3♣ は引き分け" $ Cards c_3H c_3S `compare` Cards c_3D c_3C `shouldBe` EQ
 
-      describe "ストレート同士の比較" $ do
-        it "K♠/Q♦ < K♦/A♠" $ Cards c_kS c_qD < Cards c_kD c_aS `shouldBe` True
-        it "2♦/3♠ > 2♦/A♠" $ Cards c_2D c_3S > Cards c_2D c_aS `shouldBe` True
-        it "8♦/9♠ と 9♥/8♣ は引き分け" $ Cards c_8D c_9S `compare` Cards c_9H c_8C `shouldBe` EQ
+        describe "ストレート同士の比較" $ do
+          it "K♠/Q♦ < K♦/A♠" $ Cards c_kS c_qD < Cards c_kD c_aS `shouldBe` True
+          it "2♦/3♠ > 2♦/A♠" $ Cards c_2D c_3S > Cards c_2D c_aS `shouldBe` True
+          it "8♦/9♠ と 9♥/8♣ は引き分け" $ Cards c_8D c_9S `compare` Cards c_9H c_8C `shouldBe` EQ
 
-      describe "フラッシュ同士の比較" $ do
-        it "A◆/Q◆ > J♥/K♥" $  Cards c_aD c_qD > Cards c_jH c_kH `shouldBe` True
-        it "4◆/2◆ < 3♥/5♥" $  Cards c_4D c_2D < Cards c_3H c_5H `shouldBe` True
-        it "5◆/7◆ と 5♥/7♥ は引き分け" $  Cards c_5D c_7D `compare` Cards c_5H c_7H `shouldBe` EQ
+        describe "フラッシュ同士の比較" $ do
+          it "A◆/Q◆ > J♥/K♥" $  Cards c_aD c_qD > Cards c_jH c_kH `shouldBe` True
+          it "4◆/2◆ < 3♥/5♥" $  Cards c_4D c_2D < Cards c_3H c_5H `shouldBe` True
+          it "5◆/7◆ と 5♥/7♥ は引き分け" $  Cards c_5D c_7D `compare` Cards c_5H c_7H `shouldBe` EQ
 
-      describe "ハイカード同士の比較" $ do
-        it "A◆/Q♣ > J♠/K◆" $ Cards c_aD c_qC > Cards c_jS c_kD `shouldBe` True
-        it "4◆/2♥ < 3♠/5◆" $ Cards c_4D c_2H < Cards c_3S c_5D `shouldBe` True
-        it "5◆/7♥ と 5♠/7◆ は引き分け" $ Cards c_5D c_7H `compare` Cards c_5S c_7D `shouldBe` EQ
+        describe "ハイカード同士の比較" $ do
+          it "A◆/Q♣ > J♠/K◆" $ Cards c_aD c_qC > Cards c_jS c_kD `shouldBe` True
+          it "4◆/2♥ < 3♠/5◆" $ Cards c_4D c_2H < Cards c_3S c_5D `shouldBe` True
+          it "5◆/7♥ と 5♠/7◆ は引き分け" $ Cards c_5D c_7H `compare` Cards c_5S c_7D `shouldBe` EQ
+
+  describe "スリーカード" $ do 
+    describe "役の判定" $ do
+      it "A♥とA♣とA◆ はスリーカードである" $ threeHand (ThreeCards c_aH c_aC c_aD) `shouldBe` ThreeCard
+      it "2♥と4♥と6♥ はフラッシュである" $ threeHand (ThreeCards c_2H c_4H c_6H) `shouldBe` Flush
+    
+      describe "ペアの判定" $ do 
+        it "K♥とK♣とA♥ はペアである" $ threeHand (ThreeCards c_kH c_kC c_aH) `shouldBe` Pair
+        it "2♥と3♣と2◆ はペアである" $ threeHand (ThreeCards c_2H c_3C c_2D) `shouldBe` Pair
+        it "2♥と3♣と3◆ はペアである" $ threeHand (ThreeCards c_2H c_3C c_3D) `shouldBe` Pair
+
+      describe "ストレートの判定" $ do 
+        it "A♥とK♥とQ◆ はストレートである" $ threeHand  (ThreeCards c_aH c_kH c_qD) `shouldBe` Straight 
+        it "K♥とA♥とQ◆ はストレートである" $ threeHand  (ThreeCards c_kH c_aH c_qD) `shouldBe` Straight 
+        it "2♥とA♥と3◆ はストレートである" $ threeHand  (ThreeCards c_2H c_aH c_3D) `shouldBe` Straight 
+
+        -- it "7♠と8♠ はストレートフラッシュである" $ hand (Cards c_7S c_8S) `shouldBe` StraightFlush
+      -- it "A♠とA♥ はペアである" $ hand (Cards c_aS c_aH) `shouldBe` Pair
+      -- it "K♠とA♥ はストレートである" $ hand (Cards c_kS c_aH) `shouldBe` Straight
+      -- it "7♦と8♠ はストレートである" $ hand (Cards c_7D c_8S) `shouldBe` Straight
+      -- it "A♠と7♠ はフラッシュである" $ hand (Cards c_aS c_7S) `shouldBe` Flush
+      -- it "7♦とA♥ はハイカードである" $ hand (Cards c_7D c_aH) `shouldBe` HighCard
+          
