768423c680255283deff4567dbbe33208ed8309b
Jun Nakajima <ikuta1919@gmail.com>
2019-05-19 16:10:39 +0900

    [mod] on関数を使って、Ordインスタンスの実装を簡単にした

--- src/Poker.hs
+++ src/Poker.hs
@@ -2,17 +2,30 @@ module Poker where
 
 import Data.List
 import Card
+import Data.Function
 
 instance Ord Rank where
   _ <= Ace = True
   Ace <= _ = False
   left <= right = fromEnum left <= fromEnum right
 
+-- カードの比較はランクで比較する
 instance Ord Card where
-  compare left right = rank left `compare` rank right
+  compare = compare `on` rank
 
 data Hand =  HighCard | Flush | Straight | Pair | StraightFlush
   deriving (Show, Ord, Eq)
+
+newtype TwoCardHand = TwoCardHand { fromTwoCardHand :: Hand } deriving (Eq)
+instance Ord TwoCardHand where 
+  compare  = compare `on` strength . fromTwoCardHand
+    where 
+      strength :: Hand -> Int 
+      strength StraightFlush = 5
+      strength Pair = 4
+      strength Straight = 3
+      strength Flush = 2
+      strength HighCard = 1
 
 hand :: Cards -> Hand
 hand (Cards first second)
@@ -35,15 +48,13 @@ data Cards = Cards Card Card
   deriving (Show, Eq)
 
 instance Ord Cards where
-  compare left right = case compare (hand left) (hand right) of
+  compare left right = case (compare `on` TwoCardHand . hand) left right of
     EQ -> compareCardsWithRank left right
     other -> other
 
 compareCardsWithRank :: Cards -> Cards -> Ordering
-compareCardsWithRank left right = sortedLeft `compare` sortedRight
+compareCardsWithRank = compare `on` sortCards
   where
-    sortedLeft = sortCards left
-    sortedRight = sortCards right
     sortCards :: Cards -> [Card]
     sortCards (Cards left right) = case descSortedCards of
       [ace@(Card _ Ace), two@(Card _ Two)] -> [two, ace]
