4f7ea3c86d41ac384cd36685e58d13b11b6d3802
Jun Nakajima <ikuta1919@gmail.com>
2019-05-22 00:21:25 +0900

    [mod] module分けを実施

---
+++ src/Poker/Hand.hs
@@ -0,0 +1,72 @@
+module Poker.Hand where 
+
+import Data.List
+import Card
+import Data.Function
+import Data.Ord
+
+data Hand =  HighCard | Flush | Straight | Pair | StraightFlush | ThreeCard
+  deriving (Show, Ord, Eq)
+
+class Cards a where 
+  hand :: a -> Hand
+
+instance Cards TwoCards where 
+  hand (TwoCards first second)
+    | isStraightFlush cards = StraightFlush
+    | isPair cards = Pair
+    | isStraight cards = Straight
+    | isFlush cards = Flush
+    | otherwise = HighCard
+    where
+      cards = [first, second]
+
+instance Cards ThreeCards where 
+  hand (ThreeCards first second third)
+    | isStraightFlush cards = StraightFlush
+    | isThreeCard cards = ThreeCard
+    | isStraight cards = Straight
+    | isFlush cards = Flush
+    | isPair cards = Pair
+    | otherwise = HighCard
+    where
+      cards = [first,second,third]
+
+data TwoCards = TwoCards Card Card
+  deriving (Show, Eq)
+
+data ThreeCards = ThreeCards Card Card Card
+  deriving (Show, Eq)
+
+isPair :: [Card] -> Bool
+isPair cards = or [ x `hasSameRank` y | x <- cards, y <- cards, x /= y]
+
+isFlush :: [Card] -> Bool
+isFlush (card:cards) = all (hasSameSuit card) cards
+
+isThreeCard :: [Card] -> Bool
+isThreeCard  (card:cards) = all (hasSameRank card) cards
+
+isStraight :: [Card] -> Bool
+isStraight = isConsecutiveRanks . map rank
+  where
+    isConsecutiveRanks :: [Rank] -> Bool
+    isConsecutiveRanks = or . map (scanPairs (\l r -> r - l == 1)) . orderCandidates
+
+isStraightFlush :: [Card] -> Bool
+isStraightFlush cards = isStraight cards && isFlush cards
+
+
+orderCandidates :: [Rank] -> [[Int]]
+orderCandidates ranks = map sort $ sequence $ map candidates ranks
+  where
+    candidates :: Rank -> [Int]
+    candidates Ace = [1, 14]
+    candidates x = [fromEnum x + 1]
+
+scanPairs :: (a -> a -> Bool) -> [a] -> Bool
+scanPairs f [left, right] = f left right
+scanPairs f (left:right:rest)
+  | f left right = scanPairs f (right:rest)
+  | otherwise = False
+
---
+++ src/Poker/Poker.hs
@@ -0,0 +1,72 @@
+module Poker.Poker where
+
+import Data.List
+import Card
+import Data.Function
+import Data.Ord
+import Poker.Hand
+
+instance Ord Rank where
+  _ <= Ace = True
+  Ace <= _ = False
+  left <= right = fromEnum left <= fromEnum right
+
+-- カードの比較はランクで比較する
+instance Ord Card where
+  compare = compare `on` rank
+
+newtype TwoCardHand = TwoCardHand { fromTwoCardHand :: Hand } deriving (Eq)
+instance Ord TwoCardHand where
+  compare  = compare `on` strength . fromTwoCardHand
+    where
+      strength :: Hand -> Int
+      strength StraightFlush = 5
+      strength Pair = 4
+      strength Straight = 3
+      strength Flush = 2
+      strength HighCard = 1
+
+newtype ThreeCardHand = ThreeCardHand { fromThreeCardHand :: Hand } deriving (Eq)
+instance Ord ThreeCardHand where
+  compare = compare `on` strength . fromThreeCardHand
+    where
+      strength :: Hand -> Int
+      strength StraightFlush = 6
+      strength ThreeCard = 5
+      strength Straight = 4
+      strength Flush = 3
+      strength Pair = 2
+      strength HighCard = 1
+
+instance Ord TwoCards where
+  compare = compareWithTpl (TwoCardHand . hand, orderCards)
+    where 
+      orderCards :: TwoCards -> [Card]
+      orderCards (TwoCards first second ) = orderingCardsWithHand [first, second]
+
+instance Ord ThreeCards where
+  compare = compareWithTpl (ThreeCardHand . hand, orderCards)
+    where
+      orderCards :: ThreeCards -> [Card]
+      orderCards (ThreeCards first second third) = orderingCardsWithHand [first, second, third]
+
+compareWithTpl :: (Ord a, Ord b) => (c -> a, c -> b) -> c -> c -> Ordering
+compareWithTpl (f, g) = compare `on` do
+  a <- f
+  b <- g
+  return (a, b)
+lowestRank :: [Rank] -> Rank
+lowestRank [Ace, Two] = Ace
+lowestRank [Ace, Three, Two] = Ace
+lowestRank ranks = minimum ranks
+
+-- 役によって、カード自体を並び替えして返す
+orderingCardsWithHand :: [Card] -> [Card]
+orderingCardsWithHand cards =
+  case sorted of
+    [f@(Card _ Ace), s@(Card _ Two)] -> [s, f]
+    [f@(Card _ Ace), s@(Card _ Three), t@(Card _ Two)] -> [s, t, f]
+    [f, s, t] | rank s == rank t -> [s, t, f]
+    other -> other
+    where
+      sorted = sortOn Down cards
